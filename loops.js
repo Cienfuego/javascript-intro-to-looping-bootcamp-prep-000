9f049e0001b9079046ab29ad649abf647fb43145
Michael Tompkins <mtompkins82@gmail.com>
2018-08-11 11:59:20 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -2,10 +2,29 @@ function forLoop(array){
   //array = []
   for(let i = 0; i < 25; i++){
     if(i == 1){
-      array.push("I am 1 strange loop.")
+      array.push("I am 1 strange loop.");
     }else{
-      array.push(`I am ${i} strange loops.`)
+      array.push(`I am ${i} strange loops.`);
     }
   }
-  return(array)
+  return(array);
+}
+
+function whileLoop(n){
+  while(n > 0){
+    console.log(n);
+    n--;
+  }
+  return 'done';
+}
+
+function doWhileLoop(array){
+  var i = 0
+  function incrementVariable() {
+    i = i + 1;
+  }
+  do{
+    array = array.pop();
+    incrementVariable();
+  }while (array.length > 0 && incrementVariable()) 
 }
